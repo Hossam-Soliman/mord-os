e507e78fc5332de7d372188e56bf2ae8e22d195b
Hossam-Soliman <60302905+Hossam-Soliman@users.noreply.github.com>
2021-06-05 16:13:40 +0300

    login redirections

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
 import React from "react";
 import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
-import "./App.css";
+import { useSelector } from "react-redux";
 //pages
 import HomePage from "./pages/HomePage";
 import LoginPage from "./pages/LoginPage";
 //components
 import Footer from "./components/shared/Footer";
+//selectors
+import { getIsLoggedIn } from "./store/app/AppSelectors";
 
 function App() {
+  const isLoggedIn = useSelector((state) => getIsLoggedIn({ state }));
   return (
     <BrowserRouter>
       <div className='app'>
         <Switch>
-          <Route path='/login' component={LoginPage} />
           <Route
             exact
-            path='/'
-            component={HomePage}
-            // render={() => <Redirect to='/home' />}
+            path='/home'
+            component={isLoggedIn ? HomePage : LoginPage}
+          />
+          <Route exact path='/' render={() => <Redirect to='/home' />} />
+          <Route
+            exact
+            path='/login'
+            component={isLoggedIn ? HomePage : LoginPage}
           />
         </Switch>
-        <Footer />
+        {isLoggedIn && <Footer />}
       </div>
     </BrowserRouter>
   );
